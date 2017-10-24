69e443a6fe9041644acddfc75f88def7baf7db41
DiponGhosh <diponghosh0@gmail.com>
2017-10-24 21:48:07 +0530

    cleaning code undo

--- src/GagBox.js
+++ src/GagBox.js
@@ -41,6 +41,17 @@ class GagBox extends Component {
 
   _handleScroll = (event) => {
     const gagElement = document.getElementsByClassName('gagComponent')[0];
+    console.log('a' + gagElement.clientHeight + ' b' + 
+                gagElement.scrollTop + ' c' +
+                document.body.scrollHeight + ' length ' + this.state.jokesList.length
+                );
+    /*if (
+      (document.body.clientHeight + gagElement.scrollTop) === document.body.scrollHeight &&
+      this.state.jokesList.length
+    ) {
+      this._getGag();
+    }*/
+
     const windowHeight = "innerHeight" in window ? window.innerHeight : document.gagElement.offsetHeight;
     const body = document.body;
     const docHeight = Math.max(body.scrollHeight, body.offsetHeight, gagElement.clientHeight,  gagElement.scrollHeight, gagElement.offsetHeight);
